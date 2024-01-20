c95994ea39e68b7af5935d23bdf0681972717a67
SunidhiR <102981536+SunidhiR@users.noreply.github.com>
2024-01-20 11:26:34 +0530

    Update 0141-linked-list-cycle.py

--- 0141-linked-list-cycle.py
+++ 0141-linked-list-cycle.py
@@ -1,9 +1,1 @@
-class Solution(object):
-    def hasCycle(self, head):
-        first = head
-        while first and first.next:
-            head = head.next
-            first = first.next.next
-            if head is first:
-                return True
-        return False
+
